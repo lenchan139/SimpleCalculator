94d763ff3fce0da1bbe6273344c5b010ca95bf99
lenchan139 <mail@lenchan139.org>
2016-01-21 19:17:29 +0800

    add comment

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,14 +12,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
+//This is a sub-program when you are click a "btnCalculate" button on "MainWindow" form.
 void MainWindow::on_btnCalculate_clicked()
 {
     //Create some varible
+    //You need use QString or double to create varible like: QString <varible name>
     QString strOperator,strAns;
     double dblInput1,dblInput2,dblAns;
     dblInput1 = ui->lineInput1->text().toDouble();
     dblInput2 = ui->lineInput2->text().toDouble();
+    //You need use ui->... to call the GUI element.
+    //the format is ui::[formname]->[object name]->[value name]
+    //Moreover, " ui->comboChoices->currentText()" is a QString,
+    //so that you can use this as a QString.
     strOperator = ui->comboChoices->currentText();
 
     //do if
@@ -42,6 +47,7 @@ void MainWindow::on_btnCalculate_clicked()
     else{
         strAns = "Error!";
     }
-    //QString strTemp = QString::number(dblInput1) + "!!!" + QString::number(dblInput2);
+
+    //Output, use setText(varible) to put the varible to the Output box.
     ui->lineAnswer->setText(strAns);
 }
